98b5bed891b09c53987cf4c9a2b0170d986b2818
Atul-Kumar-Official <akumarofficial4224@gmail.com>
2021-11-08 16:56:56 +0530

    [Remove]: Transition duration of navbar

--- Clones/CodeWithHarry Clone/js/navbar.js
+++ Clones/CodeWithHarry Clone/js/navbar.js
@@ -2,7 +2,6 @@ const navbar = document.querySelector('.navbar').querySelector('.main');;
 const hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', () => {
-    navbar.style.transition = "0.5s";
     if(navbar.style.display === "none"){
         navbar.style.display = "block";
     }
