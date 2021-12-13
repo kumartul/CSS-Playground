63f3aeda4a014380862be3578d9e772ef5931daa
Atul-Kumar-Official <akumarofficial4224@gmail.com>
2021-12-13 16:32:28 +0530

    [Build]: Introduction section

---
+++ TailwindCSS/Clones/ColorHub Clone/src/js/main.js
@@ -0,0 +1,7 @@
+const videoContainer = document.getElementById("video-container");
+const playBtn  = document.getElementById("play-btn");
+
+// Add an event listener to the play button
+playBtn.addEventListener('click', () => {
+     videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/KZrjhUj6AgQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`
+});
