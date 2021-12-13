14a9826167327d20165222f34a7b397b3525d5da
Atul-Kumar-Official <akumarofficial4224@gmail.com>
2021-12-13 16:32:55 +0530

    [Remove]: An extra whitespace

--- TailwindCSS/Clones/ColorHub Clone/src/js/main.js
+++ TailwindCSS/Clones/ColorHub Clone/src/js/main.js
@@ -3,5 +3,5 @@ const playBtn  = document.getElementById("play-btn");
 
 // Add an event listener to the play button
 playBtn.addEventListener('click', () => {
-     videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/KZrjhUj6AgQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`
+    videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/KZrjhUj6AgQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`
 });
